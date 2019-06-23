674560da4c65e592a34e0ca06e74ecbcebceb6cc
quanlyhocsinhcap2 <51818547+quanlyhocsinhcap2@users.noreply.github.com>
2019-06-23 10:12:50 +0700

    vietanh
    ok

--- Ung_dung_Giam_thi/Xu_ly/Xu_ly_3L.js
+++ Ung_dung_Giam_thi/Xu_ly/Xu_ly_3L.js
@@ -15,6 +15,19 @@ function Doc_Danh_sach_Hoc_sinh() {
     return Du_lieu
 }
 
+function Doc_Nhat_ky_Diem_danh() {
+    var Du_lieu = {}
+    var Xu_ly_HTTP = new XMLHttpRequest()
+    var Tham_so = `Ma_so_Xu_ly=Doc_Danh_sach_Nhat_ky`
+    var Dia_chi_Xu_ly = `${Dia_chi_Dich_vu}?${Tham_so}`
+    Xu_ly_HTTP.open("POST", Dia_chi_Xu_ly, false)
+    Xu_ly_HTTP.send("")
+    var Chuoi_JSON = Xu_ly_HTTP.responseText
+    if (Chuoi_JSON != "")
+        Du_lieu = JSON.parse(Chuoi_JSON)
+    return Du_lieu
+}
+
 function Ghi_Danh_sach_Vang(Danh_sach_Vang) {
     var Kq = ""
     var Xu_ly_HTTP = new XMLHttpRequest()
@@ -22,6 +35,18 @@ function Ghi_Danh_sach_Vang(Danh_sach_Vang) {
     var Dia_chi_Xu_ly = `${Dia_chi_Dich_vu}?${Tham_so}`
     Xu_ly_HTTP.open("POST", Dia_chi_Xu_ly, false)
     var Chuoi_goi = JSON.stringify(Danh_sach_Vang)
+    Xu_ly_HTTP.send(Chuoi_goi)
+    Kq = Xu_ly_HTTP.responseText
+    return Kq
+}
+
+function Ghi_Nhat_ky_Diem_danh(noi_dung) {
+    var Kq = ""
+    var Xu_ly_HTTP = new XMLHttpRequest()
+    var Tham_so = `Ma_so_Xu_ly=Ghi_Nhat_ky`
+    var Dia_chi_Xu_ly = `${Dia_chi_Dich_vu}?${Tham_so}`
+    Xu_ly_HTTP.open("POST", Dia_chi_Xu_ly, false)
+    var Chuoi_goi = JSON.stringify(noi_dung)
     Xu_ly_HTTP.send(Chuoi_goi)
     Kq = Xu_ly_HTTP.responseText
     return Kq
